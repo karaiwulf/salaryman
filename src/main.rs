4d3d9e0805bc5e252e2e44db1f6d67b72c5c7d76
yuzu <yuzu@b9215c17-b818-4693-b096-d1e41a411fef>
2025-05-29 06:17:28 +0000

    proof of concept: works
    git-svn-id: svn+ssh://diminuette.aengel.lesbianunix.dev/salaryman/trunk@2 b9215c17-b818-4693-b096-d1e41a411fef

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{Command, Stdio, Child};
+use std::io::Read;
+
+fn exec(image: &str, args: Vec<&str>) -> Result<Child, Box<dyn std::error::Error>> {
+    let child = Command::new(image).args(args).stdin(Stdio::piped()).stdout(Stdio::piped()).spawn()?;
+    Ok(child)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut child = exec("java", vec!["-jar", "minecraft_server.jar"])?;
+    std::thread::sleep(std::time::Duration::from_secs(60));
+    let mut buf: [u8; 512] = [0; 512];
+    child.stdout.as_mut().unwrap().read(&mut buf[..])?;
+    println!("{}", String::from_utf8_lossy(&buf));
+    child.kill()?;
+    Ok(())
 }
